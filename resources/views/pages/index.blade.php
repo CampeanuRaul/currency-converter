1748b9d42da0260504e49a50b26bd03adf091ea6
CampeanuRaul <43892814+CampeanuRaul@users.noreply.github.com>
2018-10-30 14:08:25 +0200

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,31 +2,7 @@
 
 @section('content')
 
-{{-- @foreach($data as $d)
-		<p>{{ $d->moneda }}</p>
-		<p>{{ $d->valoare }}</p>
-		<p>{{ $d->data }}</p>
-@endforeach --}}
-		
-	{{-- 	@if($result) {
-		{{ $result }}
-		}
-		@endif --}}
-
-
 {{ Form::open(['route' => ['pages.setData'], 'method' => 'POST']) }}
-{{-- {{ Form::text('value', null) }}
-<select name="currency1" >
-	@foreach($data as $d)
-		<option value="{{ $d->valoare }}">{{ $d->moneda }}</option>
-	@endforeach
-</select>
-
-<select name="currency2" >
-	@foreach($data as $d)
-		<option value="{{ $d->valoare }}">{{ $d->moneda }}</option>
-	@endforeach
-</select> --}}
 {{ Form::label('data', 'Foloseste cursul din data de:') }}
 <select name="data" >
 	@foreach($date as $d)
